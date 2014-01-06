d35fdbc41238b9988874524f661af57de13537e7
Roman Krysinski <rkrysinski@gmail.com>
2014-01-06 01:59:11 +0100

    gae: gui integration

---
+++ gui/src/main/java/com/krycha/vaadin/example/entity/Measurement.java
@@ -0,0 +1,121 @@
+/****************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package com.krycha.vaadin.example.entity;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.Extension;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Measurements entity.
+ */
+@Entity
+public class Measurement implements Serializable {
+
+	private static final long serialVersionUID = 997486721740199725L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected int id;
+
+	@Extension(vendorName="datanucleus", key="gae.unindexed", value="true")
+	@Size(min = 3, max = 10)
+	@NotNull
+	protected String shortName = "";
+
+	@Size(min = 0, max = 300)
+	protected String description = "";
+
+	@Version
+	private long version;
+
+	/**
+	 * @return the shortName
+	 */
+	public String getShortName() {
+		return shortName;
+	}
+
+	/**
+	 * @param shortName
+	 *            the shortName to set
+	 */
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the version
+	 */
+	public long getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param version
+	 *            the version to set
+	 */
+	public void setVersion(long version) {
+		this.version = version;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return shortName;
+	}
+
+}
